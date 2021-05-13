1eee96b2df47ab8e2826d97ad0f9501378c358e6
Doug Hammer <doug.hammer16@gmail.com>
2021-05-12 20:26:18 -0700

    created fixed grid container/started on buttons to change grid size

--- script.js
+++ script.js
@@ -1,12 +1,37 @@
 const container = document.querySelector('.grid-container')
 
-const makeRows = (rows, cols) => {
-  container.style.setProperty('--grid-rows', rows);
-  container.style.setProperty('--grid-cols', cols);
-  for(let c = 0; c < (rows * cols); c++) {
+function createGrid(size = 33 * 45) {
+  for(let c = 0; c < size; c++) {
     let cell = document.createElement('div');
     container.appendChild(cell).className = 'grid-item';
+    cell.addEventListener('mouseover', e => {
+      e.target.style.backgroundColor = 'grey';
+    })
   };
+  container.style.gridTemplateColumns = `repeat(${45}, auto)`;
+  container.style.gridTemplateRows = `repeat(${33}, auto)`;
 };
+createGrid();
 
-makeRows(16, 16);
+function changeSize() {
+  const large = 11 * 15;
+  const medium = 33 * 45;
+  const small = 65 * 90;
+
+}
+
+let sizeBtns = document.querySelectorAll('#btns > button');
+sizeBtns = [...sizeBtns];
+
+sizeBtns.forEach(btn => {
+  btn.addEventListener('click', e => {
+    if(e.target.classList.contains('sm-btn')) {
+      console.log('thats sm.')
+    } else if (e.target.classList.contains('med-btn')) {
+      console.log('thats med');
+    } else if (e.target.classList.contains('lg-btn')) {
+      console.log('thats lg!');
+    }
+  });
+});
+
